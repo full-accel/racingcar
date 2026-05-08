b195302ce5b95cec5443bdc554ff894999cdbdf7
chan-nni <rkdcksal02@gmail.com>
2026-05-08 16:49:24 +0900

    feat: 입출력 뷰(View) 인터페이스 및 랜덤 유틸리티 추가

---
+++ src/main/java/com/fullaccel/view/InputView.java
@@ -0,0 +1,13 @@
+package com.fullaccel.view;
+
+public class InputView {
+    public String readCarInfo() {
+        // TODO: "자동차 이름과 차종을 입력하세요 (예: pobi:SportsCar, woni:Truck, jun:Basic)" 출력 및 입력 받기
+        return null;
+    }
+
+    public int readTryCount() {
+        // TODO: "시도할 횟수는 몇 회인가요?" 출력 및 숫자 입력 받기
+        return 0;
+    }
+}
