0b550f1bbf63a586b1162ffff0a036bfec9abff8
chan-nni <rkdcksal02@gmail.com>
2026-05-08 16:51:12 +0900

    feat: 게임 컨트롤러 및 메인 실행부(Application) 구성

--- src/main/java/com/fullaccel/Application.java
+++ src/main/java/com/fullaccel/Application.java
@@ -1,5 +1,7 @@
 package com.fullaccel;
 
 public class Application {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        // TODO: 프로그램의 시작점
+    }
 }
